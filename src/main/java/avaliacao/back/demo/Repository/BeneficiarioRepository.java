eb7e277293aa56760dcb36d20b1a34115d3aabaf
Norton Manfré <nortonmanfrejr@hotmail.com>
2024-07-19 16:42:32 -0300

    feat: Repository and query where catch all docs off all beneficiario by id beneficiario

---
+++ src/main/java/avaliacao/back/demo/Repository/BeneficiarioRepository.java
@@ -0,0 +1,9 @@
+package avaliacao.back.demo.Repository;
+
+import avaliacao.back.demo.Model.Beneficiario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeneficiarioRepository extends JpaRepository<Beneficiario, Long> {
+}
---
+++ src/main/java/avaliacao/back/demo/Repository/DocumentoRepository.java
@@ -0,0 +1,16 @@
+package avaliacao.back.demo.Repository;
+
+import avaliacao.back.demo.Model.Documento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DocumentoRepository extends JpaRepository<Documento, Long> {
+
+    @Query("SELECT * FROM DOCUMENTO WHERE beneficiario = :beneficiario")
+    public List<Documento> GetAllByBeneficiario(@Param("beneficiario") Long beficiario);
+}
