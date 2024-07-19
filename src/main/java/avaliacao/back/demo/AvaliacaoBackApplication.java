eb6c41e84247e041c3c66765d6532db14a69ec73
Norton Manfré <nortonmanfrejr@hotmail.com>
2024-07-19 16:44:35 -0300

    feat: application.prop and many resources

---
+++ src/main/java/avaliacao/back/demo/AvaliacaoBackApplication.java
@@ -0,0 +1,13 @@
+package avaliacao.back.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoBackApplication.class, args);
+	}
+
+}
